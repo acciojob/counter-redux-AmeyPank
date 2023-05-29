44a34cb5969c0b6ab37c854be3baba8ad65e72a5
AmeyPank <pankantiameya@gmail.com>
2023-05-30 00:56:30 +0530

    Now Check

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import Counter from './components/Counter';
 import counterReducer from './reducers/reducer';
+import App from './components/App';
 
 
 const store = createStore(counterReducer);
 
 ReactDOM.render(
     <Provider store={store}>
-        <Counter />
+        <App />
     </Provider>,
     document.getElementById('root')
 );
